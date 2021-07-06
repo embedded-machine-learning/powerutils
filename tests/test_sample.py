fe64ba8eae66234444cb1921c266495633d39dfc
mwessley <matthias.wess@gmail.com>
2021-07-06 14:38:09 +0200

    updated test function

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,21 @@
 from powerutils import measurement
+import time
 
-def test_pass():
+def test_power():
+    pm = measurement.power_measurement(sampling_rate=500000, data_dir="tests/tmp", max_duration=60)
+
+    # print(pm.__dict__)
+    test_kwargs =  {"model_name": "vgg16"}
+
+    pm.start_gather(test_kwargs)
+    print("do something")
+    time.sleep(2)
+    pm.end_bench(True) # stop the power measurement
 
     assert True, "power measurement passed"
+
+def main():
+    test_power()
+
+if __name__ == "__main__":
+    main()
